d1d410cad33fca368bd08f9c7600eb9eba2440ec
HadiHeikal <hadiiheikal@gmail.com>
2026-04-17 14:51:38 +0200

    root update

--- lib/root.dart
+++ lib/root.dart
@@ -25,115 +25,139 @@ class _RootState extends State<Root> {
   void initState() {
     super.initState();
     screens = [
-    // 1- Selection
-    SingleSelect(),
-    MultiImageSelect(),
-    ToggleSelect(),
-    MultiSelect(),
+      // 1- Selection
+      SingleSelect(),
+      MultiImageSelect(),
+      ToggleSelect(),
+      MultiSelect(),
 
-    // 2- Uploading
-    UploadImage(),
-    UploadMultiImage(),
-    UploadVideos(),
-    UploadFiles(),
+      // 2- Uploading
+      UploadImage(),
+      UploadMultiImage(),
+      UploadVideos(),
+      UploadFiles(),
 
-    // 3- Searching
-    SearchFeature(),
+      // 3- Searching
+      SearchFeature(),
 
-    // 4- Form Validation
-    FormValidation(),
-  ];
-  int currentIndex = 0;
+      // 4- Form Validation
+      FormValidation(),
+    ];
+    int currentIndex = 0;
+    @override
+    void initState() {
+      super.initState();
+      screens = [
+        // 1- Selection
+        SingleSelect(),
+        MultiImageSelect(),
+        ToggleSelect(),
+        MultiSelect(),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(),
-        controller: pageController,
-        children: screens,
-        onPageChanged: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                // back
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      pageController.jumpToPage(
-                        // if we are on the first page, go to the last page, else go to the previous page
-                        currentIndex == 0
-                            ? screens.length - 1
-                            : currentIndex - 1,
-                      );
-                    });
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
-                      size: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(width: 14),
-                // next
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      pageController.jumpToPage(
-                        // if we are on the last page, go to the first page, else go to the next page
-                        currentIndex == screens.length - 1
-                            ? 0
-                            : currentIndex + 1,
-                      );
-                    });
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    height: 40,
-                    decoration: BoxDecoration(color: Colors.black),
-                    child: Row(
-                      children: [
-                        Text(
-                          'next page',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        SizedBox(width: 14),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.white,
-                          size: 15,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+        // 2- Uploading
+        UploadImage(),
+        UploadMultiImage(),
+        UploadVideos(),
+        UploadFiles(),
+
+        // 3- Searching
+        SearchFeature(),
+
+        // 4- Form Validation
+        FormValidation(),
+      ];
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: PageView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: screens,
+          onPageChanged: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
-      ),
-    );
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  // back
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        pageController.jumpToPage(
+                          // if we are on the first page, go to the last page, else go to the previous page
+                          currentIndex == 0
+                              ? screens.length - 1
+                              : currentIndex - 1,
+                        );
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.black,
+                        size: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 14),
+                  // next
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        pageController.jumpToPage(
+                          // if we are on the last page, go to the first page, else go to the next page
+                          currentIndex == screens.length - 1
+                              ? 0
+                              : currentIndex + 1,
+                        );
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      height: 40,
+                      decoration: BoxDecoration(color: Colors.black),
+                      child: Row(
+                        children: [
+                          Text(
+                            'next page',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(width: 14),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
